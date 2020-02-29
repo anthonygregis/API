19b94a2392a9ac5893f7a2f7d973c8d27857762b
thedevtron <anthony@thedevtron.com>
2020-02-29 10:41:25 -0500

    Port Changes

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(3000, () => {
-    console.log("Server is up and listening on 3000...")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log("Server is up and listening on: " + PORT)
 })
 
 
