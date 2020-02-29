233f517c4608821bdf0099e9f8f8ec7f57a3a5f0
thedevtron <anthony@thedevtron.com>
2020-02-28 20:33:16 -0500

    Debug

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -8,13 +8,13 @@ module.exports = app => {
     app.get("/users", users.findAll);
   
     // Retrieve a single Customer with customerId
-    app.get("/users/:customerId", users.findOne);
+    app.get("/users/:userUuid", users.findOne);
   
     // Update a Customer with customerId
-    app.put("/users/:customerId", users.update);
+    app.put("/users/:userUuid", users.update);
   
     // Delete a Customer with customerId
-    app.delete("/users/:customerId", users.delete);
+    app.delete("/users/:userUuid", users.delete);
   
     // Create a new Customer
     app.delete("/users", users.deleteAll);
