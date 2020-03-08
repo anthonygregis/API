983b811dee563bf13fa972a809f9928ae3c28a0a
thedevtron <anthony@thedevtron.com>
2020-03-08 17:40:02 -0400

    Add Logging

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+import LogRocket from 'logrocket';
+LogRocket.init('zwqirp/api');
 const PORT = process.env.PORT || 3000
 
 const app = express();
